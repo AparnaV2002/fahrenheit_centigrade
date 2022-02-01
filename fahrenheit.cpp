8dd83d5925ae5a30f05193b2b49c8fc0ff3be35b
APARNA V <95341493+AparnaV2002@users.noreply.github.com>
2022-02-01 22:44:57 +0530

    Create fahrenheit.cpp

---
+++ fahrenheit.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   double centigrade, fehrenheit;
+cout << "GIVE TEMPERATURE IN CENTIGRAGE:";
+cin >> centigrade;
+fehrenheit = centigrade * 9/5 +32;
+cout << " IN FAHRENHEIT :"<< fehrenheit <<endl;
+}
